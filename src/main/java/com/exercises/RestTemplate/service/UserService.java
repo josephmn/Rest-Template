cbe0d119332cbf90adf06e5796f68e0128084f1a
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-09-21 14:43:10 -0500

    limpiando codigo

--- src/main/java/com/exercises/RestTemplate/service/UserService.java
+++ src/main/java/com/exercises/RestTemplate/service/UserService.java
@@ -1,9 +1,7 @@
 package com.exercises.RestTemplate.service;
 
 import com.exercises.RestTemplate.dto.UserDTO;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
